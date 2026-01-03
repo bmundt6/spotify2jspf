07630fe9bf5c07d364f2b514e33eef3688b75e1c
Benjamin Mundt <benMundt@ibm.com>
2026-01-03 18:08:58 -0500

    Implement MBID lookup by Spotify URL

--- spotify2jspf.sh
+++ spotify2jspf.sh
@@ -55,11 +55,16 @@ done
 
 if [[ $verbose ]]; then
     _msg () {
-        echo "$*"
+        >&2 echo "$*"
     }
 else
     _msg () { :; }
 fi
+
+_failmsg () {
+    _msg "$@"
+    echo 1
+}
 
 if [[ -z $in_file ]]; then
     in_file="./Playlist1.json"
@@ -80,6 +85,23 @@ playlist_dicts=()
 
 mapfile playlist_dicts < <(jq -c '.playlists[]' "$in_file")
 
+_map_track() { # return MBID for the given spotify URL, if any
+    spotify_track_url=$1
+    curl_cmd=(curl -s "https://musicbrainz.org/ws/2/url?fmt=json&resource=${spotify_track_url}&inc=recording-rels")
+    for ii in {1..5}; do
+        # up to 5 tries in case we get some network issues
+        res=$("${curl_cmd[@]}")
+        curl_rc=$?
+        ((curl_rc)) || break
+    done
+    ((curl_rc)) && return $(_failmsg "      MusicBrainz database query failed. Request='${curl_cmd[*]}'; Response (rc=$curl_rc)='$res'")
+    # if we got nothing for this URL, give up
+    #COMBAK: try harder (look up by artist/title e.g.)
+    (($(jq '.relations | length' <<<$res) >= 1)) || return $(_failmsg "      Zero back-links found for spotify track URL: ${spotify_track_url}. MusicBrainz recording query result: ${res}")
+    # if we get multiple results, we'll assume the first one is desired
+    jq -r '.relations[0].recording.id' <<<$res
+}
+
 for playlist_json in "${playlist_dicts[@]}"; do
     name_raw=$(jq -r .name <<<$playlist_json)
     name=$(_percent_encode_unsafe_chars <<<$name_raw)
@@ -96,20 +118,36 @@ for playlist_json in "${playlist_dicts[@]}"; do
     mapping_failures=()
     _msg "  Attempting to map ${#track_dicts[@]} tracks..."
     for track_json in "${track_dicts[@]}"; do
-        #TODO: map each Spotify track ID to an ISRC code, then to a MusicBrainz recording ID
+        # map each Spotify track ID to a MusicBrainz recording ID
         # (this is the "identifier" field)
         title=$(jq -r .track.trackName <<<$track_json)
-        mapped_tracks+=("$(jq -c --arg title "$title" '{
-            "title": $title,
-            "identifier": .track.trackUri,
-            "creator": .track.artistName,
-            "extension": {
-                "https://musicbrainz.org/doc/jspf#track": {
-                    "added_at": .addedDate,
+        creator=$(jq -r .track.artistName <<<$track_json)
+        added_at=$(jq -r .addedDate <<<$track_json)
+        spotify_track_uri=$(jq -r .track.trackUri <<<$track_json)
+        spotify_track_id=${spotify_track_uri##*:}
+        spotify_track_url="https://open.spotify.com/track/${spotify_track_id}"
+        display_name="$creator - $title"
+        _msg "    MAPPING: $display_name (URL: $spotify_track_url)..."
+        if mapped_track_mbid=$(_map_track "$spotify_track_url"); then
+            mapped_tracks+=("$(jq -nc \
+                --arg title "$title" \
+                --arg identifier "$mapped_track_mbid" \
+                --arg creator "$creator" \
+                --arg added_at "$added_at" \
+            '{
+                "title": $title,
+                "identifier": $identifier,
+                "creator": $creator,
+                "extension": {
+                    "https://musicbrainz.org/doc/jspf#track": {
+                        "added_at": $added_at,
+                    },
                 },
-            },
-        }' <<<$track_json)")
-        _msg "    SUCCESS: $title"
+            }' <<<$track_json)")
+            _msg "      SUCCESS"
+        else
+            _msg "      FAILURE"
+        fi
     done
     _msg "  ...done. Mapped ${#mapped_tracks[@]} of ${#track_dicts[@]} tracks."
     >"$out_fn" jq '{
